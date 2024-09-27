90a2593b2a723ee80ed364d724b2e80932872a6f
Erdem <rifaterdemsahin@gmail.com>
2024-09-27 17:15:25 +0100

    Update and rename setupcommands.sh to commands.sh

--- code/commands.sh
+++ code/commands.sh
@@ -1,5 +1,10 @@
+# Reinstall
 helm upgrade my-release oci://registry-1.docker.io/bitnamicharts/grafana -f values.yaml
+
+# Reinstall
+helm uninstall my-release 
+
+# Monitor
 kubectl logs <grafana-pod-name>
 kubectl describe pod <grafana-pod-name>
 kubectl get pvc
-
