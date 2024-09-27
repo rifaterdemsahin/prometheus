5c00b6644595ada82cb33b95f105835b899bf850
Erdem <rifaterdemsahin@gmail.com>
2024-09-27 17:11:09 +0100

    Update setupcommands.sh

--- code/setupcommands.sh
+++ code/setupcommands.sh
@@ -1,1 +1,5 @@
- helm install my-release oci://registry-1.docker.io/bitnamicharts/grafana -f values.yaml
+helm upgrade my-release oci://registry-1.docker.io/bitnamicharts/grafana -f values.yaml
+kubectl logs <grafana-pod-name>
+kubectl describe pod <grafana-pod-name>
+kubectl get pvc
+
